3d1b51996192fff84675b1f173dafd731ae0b48a
Puratchidasan Munusami <puratchidasan@gmail.com>
2021-03-28 03:41:24 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,11 @@
-import { Component, VERSION, OnInit, ViewEncapsulation } from "@angular/core";
-import tt from "@tomtom-international/web-sdk-maps";
+import { Component} from '@angular/core';
+
 
 @Component({
-  selector: "my-app",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
-  encapsulation: ViewEncapsulation.None
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  name = "TomTom Maps "; // + VERSION.major;
-  ngOnInit() {
-    var HQ = { lat: 50.8326, lng: 4.389 };
-    const map = tt.map({
-      key: "Bk1jIjy8ZLFAx79H9gtW22Xv3eIngLRo",
-      container: "map",
-      center: HQ,
-      zoom: 10
-    });
+export class AppComponent {
 
-    //var marker = new tt.Marker().setLngLat(HQ).addTo(map);
-    //var popup = new tt.Popup({ anchor: "top" }).setText("HeadQuarters");
-    //marker.setPopup(popup).togglePopup();
-
-    map.addControl(new tt.FullscreenControl());
-    map.addControl(new tt.NavigationControl());
-  }
 }
